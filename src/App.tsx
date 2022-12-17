0f54b207cdbaac50f4635ecaffbc5ea5528b0d9e
Rob Copeland <copexrob@amazon.com>
2022-12-17 17:34:57 -0500

    layout grid first pass

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((num) => num + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="main-layout">
+      <div className="main-layout__header">header</div>
+      <div className="main-layout__prev">prev</div>
+      <div className="main-layout__hero">hero</div>
+      <div className="main-layout__next">next</div>
+      <div className="main-layout__controls">controls</div>
+      <div className="main-layout__gallery">gallery</div>
     </div>
   );
 }
